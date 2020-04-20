6bc135117136369e12f827cc93cdb773da42c856
Ike Kilinc <ahmetyavuzkilinc@gmail.com>
2020-04-20 17:16:54 -0400

    iOS precursor

---
+++ iOS/InFrame/InFrame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InFrame
+//
+//  Created by Ike Kilinc on 4/17/20.
+//  Copyright © 2020 Ike Kilinc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
