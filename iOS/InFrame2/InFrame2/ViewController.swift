28c5567563d4505ef81172e699733d0a193dde3d
Ike Kilinc <ahmetyavuzkilinc@gmail.com>
2020-04-22 03:40:27 -0400

    iOS core functionality working without Bluetooth messages

--- iOS/InFrame2/InFrame2/ViewController.swift
+++ iOS/InFrame2/InFrame2/ViewController.swift
@@ -7,37 +7,84 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     
     // IDLE, DISPLAYING, TRACKING
     var state = "IDLE"
     
+    // Core image filepaths
+    let blankImg = "blank_white"
+    let loadingImg = "loading_white"
+    let testImg = "object_detection_test"
+    
     @IBOutlet weak var frameView: UIImageView!
     @IBOutlet weak var instructionLabel: UILabel!
     @IBOutlet weak var requestButton: UIButton!
     @IBOutlet weak var bottomButton: UIButton!
+    @IBOutlet weak var targetSelection: UILabel!
+    @IBOutlet weak var targetSelector: UIStepper!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Set background
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
+        
+        // Set frameView settings and starting image
+        frameView.image = UIImage(named: blankImg)
+        frameView.layer.masksToBounds = true
+        frameView.layer.borderWidth = 3.0
+        frameView.layer.borderColor = UIColor.black.cgColor
+        frameView.layer.cornerRadius = 5
+        
+        // Round out buttons
+        requestButton.layer.cornerRadius = 10
+        bottomButton.layer.cornerRadius = 10
+        
+        // Set up first instruction
+        instructUser(msg: "Request frame to begin target selection")
     }
 
     @IBAction func actionRequested(_ sender: Any) {
         if state == "IDLE" {
-            // Send Bluetooth request "GetTargets" to CSM
-            print("Frame Requested!")
-        
-            // Wait for image to be received
-        
-            // Display image
-            frameView.image = UIImage()
+            // Send Bluetooth request "GetTargets" to CSM and display the received frame
+            frameView.image = UIImage(named: requestFrameViaBluetooth())
+            
+            // Enable stepper and label
+            targetSelection.isEnabled = true
+            targetSelector.isEnabled = true
+            
+            // Update user instruction
+            instructUser(msg: "Scale to target ID and confirm, or request another frame")
         
             // Update system state
             state = "DISPLAYING"
             
+        } else if state == "DISPLAYING" {
+            // Send Bluetooth request "GetTargets" to CSM and display the received frame
+            frameView.image = UIImage(named: requestFrameViaBluetooth())
+            
+            // Reset DISPLAYING view
+            targetSelector.value = 0
+            targetSelection.text = "No Target"
+            bottomButton.isEnabled = false
+            
         } else if state == "TRACKING" {
-            print("hi")
+            // Reset image display
+            frameView.image = UIImage(named: blankImg)
+            
+            // Reset Action Request button
+            requestButton.setTitle("Request Frame", for: UIControl.State.normal)
+            
+            // Reset target selection
+            targetSelection.text = "No Target"
+            
+            // Update user instruction
+            instructUser(msg: "Select 'Stop Recording' to terminate tracking or restart")
+            
+            state = "IDLE"
         }
     }
     
@@ -47,9 +94,47 @@ class ViewController: UIViewController {
         // Send target information to CSM for tracking
         
         // Change top button name to "Stop Tracking"
+        requestButton.setTitle("Stop Tracking", for: UIControl.State.normal)
         
-        // Update confirm button to Disabled
+        // Transition to TRACKING view
+        targetSelector.value = 0
+        bottomButton.isEnabled = false
+        targetSelection.isEnabled = false
+        targetSelector.isEnabled = false
+        state = "TRACKING"
     }
     
+    
+    @IBAction func targetSelectionChanged(_ sender: Any) {
+        let currVal = Int(round(targetSelector.value))
+        // TODO: Check if currVal exists as an Instance value for any detection in currently displayed frame. If not, reset currVal to last valid selection (essentially not allowing switch to invalid selection).
+        bottomButton.isEnabled = true
+        targetSelection.text = "Target: " + String(currVal)
+    }
+    
+    
+    func instructUser(msg: String) {
+        instructionLabel.text = msg
+    }
+    
+    func requestFrameViaBluetooth() -> String {
+        /*
+         Simulates a frame request to the CSM via Bluetooth.
+         
+         :returns: String - name of frame file saved locally.
+         */
+        print("Frame Requested!")
+        
+        // Temporarily display loading image
+//        frameView.image = UIImage(named: loadingImg)
+        
+        // Simulate wait
+//        sleep(1)
+        
+        print("Frame Retrieved!")
+        
+        return testImg
+    }
+
 }
 
